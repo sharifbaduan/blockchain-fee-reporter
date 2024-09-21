d4b227662b67044924fab960a02563db6cb2add1
Sharif Baduan <sharifbaduan@capitole-consulting.com>
2024-09-21 18:39:18 +0200

    Clean app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { WinstonModule } from 'nest-winston';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FeeReportingService } from './services/fee-reporting.service';
 import { FeeReportingSchedule } from './schedules/fee-reporter.schedule';
 import winstonTransporter from './transporters/winston.transporter';
@@ -21,9 +19,7 @@ import { BnbFeeCalculationService } from './services/crypto/bnb-fee-calculation.
     }),
     HttpModule,
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     FeeReportingSchedule,
     FeeReportingService,
     BitcoinFeeRetrievalService,
