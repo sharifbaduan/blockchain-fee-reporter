07e4f9a0084fb9c02c0d746d27d74eb09136bd85
Sharif Baduan <sharifbaduan@capitole-consulting.com>
2024-09-21 18:33:03 +0200

    Enhance error handling

--- src/schedules/fee-reporter.schedule.ts
+++ src/schedules/fee-reporter.schedule.ts
@@ -1,28 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { CRYPTO_CONFIG } from 'src/config/crypto.config';
 import { FeeServiceFactoryService } from 'src/factories/fee-service-factory.service';
 
 @Injectable()
 export class FeeReportingSchedule {
-  constructor(private readonly feeServiceFactory: FeeServiceFactoryService) {}
+  constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: Logger,
+    private readonly feeServiceFactory: FeeServiceFactoryService,
+  ) {}
 
   @Cron(CronExpression.EVERY_10_SECONDS)
   async handleCron() {
     for (const crypto of CRYPTO_CONFIG) {
-      const feeRetrievalService =
-        this.feeServiceFactory.createFeeRetrievalService(crypto.unit);
+      try {
+        const feeRetrievalService =
+          this.feeServiceFactory.createFeeRetrievalService(crypto.unit);
 
-      const feeCalculationService =
-        this.feeServiceFactory.createFeeCalculationService(crypto.unit);
+        const feeCalculationService =
+          this.feeServiceFactory.createFeeCalculationService(crypto.unit);
 
-      const fee = feeCalculationService.calculateFee(
-        await feeRetrievalService.fetchFeeData(),
-      );
+        const fee = feeCalculationService.calculateFee(
+          await feeRetrievalService.fetchFeeData(),
+        );
 
-      this.feeServiceFactory
-        .createFeeReportingService()
-        .reportFee(crypto.name, fee, crypto.unit);
+        this.feeServiceFactory
+          .createFeeReportingService()
+          .reportFee(crypto.name, fee, crypto.unit);
+      } catch (error) {
+        this.logger.error(`Failed to process ${crypto.name}: ${error.message}`);
+      }
     }
   }
 }
