0fb8ea6d23c3a1f91482060c08e8795fa2149384
Sharif Baduan <sharifbaduan@capitole-consulting.com>
2024-09-22 22:18:28 +0200

    Dockerize app and use redis for cache in docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 import { ScheduleModule } from '@nestjs/schedule';
 import { WinstonModule } from 'nest-winston';
 import { FeeReportingService } from './services/fee-reporting.service';
@@ -12,9 +13,18 @@ import { BitcoinFeeCalculationService } from './services/crypto/bitcoin-fee-calc
 import { BnbFeeRetrievalService } from './services/crypto/bnb-fee-retrieval.service';
 import { BnbFeeCalculationService } from './services/crypto/bnb-fee-calculation.service';
 
+const cacheModuleConfig =
+  process.env.REDIS_HOST && process.env.REDIS_PORT
+    ? {
+        store: redisStore,
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+      }
+    : {};
+
 @Module({
   imports: [
-    CacheModule.register(),
+    CacheModule.register<any>(cacheModuleConfig),
     ScheduleModule.forRoot(),
     WinstonModule.forRoot({
       transports: winstonTransporter,
